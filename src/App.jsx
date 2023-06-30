98a61ac5b071fd6e5f5cba946b1d878bb01b9bbd
lehung2022 <hung.le.programmer@gmail.com>
2023-06-30 23:58:44 +0700

    fixed chapter 20

--- src/App.jsx
+++ src/App.jsx
@@ -67,22 +67,38 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = async (e) => {
+  // handleSubmit v1
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const id = posts?.length ? posts[posts?.length - 1].id + 1 : 1;
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    try {
-      const response = await api.post('/posts', newPost);
-      const allPosts = [...posts, response.data];
-      setPosts(allPosts);
-      setPostTitle('');
-      setPostBody('');
-      navigate('/');
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/');
   }
+  // handleSubmit v1
+
+  // handleSubmit v2
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const id = posts?.length ? posts[posts?.length - 1].id + 1 : 1;
+  //   const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+  //   const newPost = { id, title: postTitle, datetime, body: postBody };
+  //   try {
+  //     const response = await api.post('/posts', newPost);
+  //     const allPosts = [...posts, response.data];
+  //     setPosts(allPosts);
+  //     setPostTitle('');
+  //     setPostBody('');
+  //     navigate('/');
+  //   } catch (err) {
+  //     console.log(`Error: ${err.message}`);
+  //   }
+  // }
+  // handleSubmit v2
 
   const handleEdit = async (id) => {
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
